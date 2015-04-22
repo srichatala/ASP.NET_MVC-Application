68eb0fadc414939178ff85ec925ea1e046c64d9c
srichatala <sri2vas@gmail.com>
2015-04-22 14:20:00 -0400

    new controller created

---
+++ CollegeApp/CollegeApp/Controllers/XyzController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    public class XyzController : Controller
+    {
+        // GET: Xyz
+        public string Index()
+        {
+            return "Welcome to MVC Application";
+        }
+    }
+}
