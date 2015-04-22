788ca96c2c826b4ced4564c3b535ae07f6879c4c
srichatala <sri2vas@gmail.com>
2015-04-22 15:45:46 -0400

    Model and view added to the controller

--- CollegeApp/CollegeApp/Controllers/XyzController.cs
+++ CollegeApp/CollegeApp/Controllers/XyzController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CollegeApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,23 @@ namespace CollegeApp.Controllers
 {
     public class XyzController : Controller
     {
+       
         // GET: Xyz
-        public string Index()
+        public ActionResult Index()
         {
-            return "Welcome to MVC Application";
+            Student Stu1 = new Student();
+            Stu1.Name = "Sinivas";
+            Stu1.Address = "Milton";
+            Stu1.PhoneNo = "647-786-6026";
+            Student Stu2 = new Student();
+            Stu2.Name = "Venkat";
+            Stu2.Address = "Toronto";
+            Stu2.PhoneNo = "647-786-0000";
+             Student Stu3 = new Student();
+            Stu3.Name = "Balaram";
+            Stu3.Address = "Ongole";
+            Stu3.PhoneNo = "647-786-0007";
+            return View(Stu3);
         }
     }
 }
---
+++ CollegeApp/CollegeApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CollegeApp.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
