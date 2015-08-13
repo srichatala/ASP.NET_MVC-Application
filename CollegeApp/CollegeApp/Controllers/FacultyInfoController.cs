8139b1c440e3526ce698d190872356739ab9c05a
srichatala <sri2vas@gmail.com>
2015-08-13 12:21:13 -0400

    facultyinfo controller added

---
+++ CollegeApp/CollegeApp/Controllers/FacultyInfoController.cs
@@ -0,0 +1,19 @@
+﻿using CollegeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    public class FacultyInfoController : Controller
+    {
+        CollegeContext db = new CollegeContext();
+        // GET: FacultyInfo
+        public ActionResult Index()
+        {
+            return View(db.Faculties.ToList());
+        }
+    }
+}
