2bb21da9869fd919d29c08c9f98c79e41971c29e
srichatala <sri2vas@gmail.com>
2015-08-27 11:09:46 -0400

    display names added in faculty model

--- CollegeApp/CollegeApp/Models/Faculty.cs
+++ CollegeApp/CollegeApp/Models/Faculty.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,8 +11,10 @@ namespace CollegeApp.Models
     {
         public int FacultyID { get; set; }
         [Required]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
         [Required]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
         [Required]
         public int Age { get; set; }
