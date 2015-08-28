61a0b66e234d664cd0c747d27f80fe16acbc68a4
srichatala <sri2vas@gmail.com>
2015-08-28 08:28:14 -0400

    coopreport context created to connect database by using entity framework

---
+++ Coopreport/Coopreport/Models/CoopreportContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Coopreport.Models
+{
+    public class CoopreportContext : DbContext
+    {
+        public DbSet<Registration> Registrations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+        }
+    }
+}
