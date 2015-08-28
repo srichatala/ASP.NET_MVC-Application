846986826489704ae7d57e2c3ffc01eafdb1ca1f
srichatala <sri2vas@gmail.com>
2015-08-28 01:15:47 -0400

    Registration model added

---
+++ Coopreport/Coopreport/Models/Registration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coopreport.Models
+{
+    public class Registration
+    {
+        public int RegistrationID { get; set; }
+        public Int64 StudentID { get; set; }
+        public string SFName { get; set; }
+        public string SLName { get; set; }
+        public string SProgram { get; set; }
+        public string SEmail { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string SProvience { get; set; }
+        public string PostalCode { get; set; }
+        public string SPhone { get; set; }
+        public string SCell { get; set; }
+        public string SPassword { get; set; }
+
+    }
+}
