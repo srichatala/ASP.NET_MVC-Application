aeba58781f5bcea6e64577031cadbe87b283599a
srichatala <sri2vas@gmail.com>
2015-08-29 00:39:16 -0400

    Display name appended to Advisor model

--- Coopreport/Coopreport/Models/Advisor.cs
+++ Coopreport/Coopreport/Models/Advisor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,17 +11,24 @@ namespace Coopreport.Models
     {
         public int AdvisorID { get; set; }
         [Required()]
+        [DisplayName("Advisor Name")]
         public string AdvisorName { get; set; }
         [Required()]
+        [DisplayName("Email ID")]
         public string AEmail { get; set; }
+        [DisplayName("Phone Number")]
         public string APhone { get; set; }
+        [DisplayName("Cell Number")]
         public string ACell { get; set; }
+        [DisplayName("Area")]
         public string AArea { get; set; }
         [Required]
         [StringLength(9)]
+        [DisplayName("User Name")]
         public string AUsername { get; set; }
         [Required()]
         [DataType(DataType.Password)]
+        [DisplayName("Password")]
         public string APassword { get; set; }
     }
 }
