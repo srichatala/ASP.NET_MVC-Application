91587891c6fe77915de37402a33c6eeee32b6bd5
srichatala <sri2vas@gmail.com>
2016-02-02 23:48:29 -0500

    Coopreport: Student controller added to do unit test

---
+++ Coopreport/Coopreport.Tests/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Coopreport;
+using Coopreport.Controllers;
+
+namespace Coopreport.Tests.Controllers
+{
+    [TestClass]
+    public class StudentController
+    {
+    }
+}
